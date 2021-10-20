4ee731b69c599053857a5f6ca13a6c8c24e2beed
BuildTools <unconfigured@null.spigotmc.org>
2021-10-20 17:19:02 -0400

    comparison logic works

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-name = "Trevor"
-eyeColor = "blue"
-heightInFeet = 6
-hairColour = "reddish"
+from random import randint
 
-print("================================")
-print("My name is " + name + " and I have " + eyeColor + " eyes")
-print("I'm about " + str(heightInFeet) + " feet tall and have " + hairColour + "hair.")
-print("I look a bit like a Fraggle, actually.")
-print("================================")
+# player will be the weapon the player chooses via input
+player = input("Choose your weapon: Rock, Paper, or Scissors: ")
+
+#instatiating the choices
+weapon = ["rock", "paper", "scissors"]
+
+#make the ai choose
+CPU = weapon[randint(0, 2)]
+
+#confirming player input
+print("player chose: " + player)
+print("the computer chose: " + CPU)
+
+#compare the two
+if (CPU == player):
+	# tie - nothing else to compare, so it'll exit
+	print("Tie! Try again")
+elif (player == "rock"):
+	if (CPU == "paper"):
+		print("you lose!")
+	else:
+		print("you win!")
+elif (player == "paper"):
+	if (CPU == "scissors"):
+		print("you lose!")
+	else:
+		print("you win!")
+elif (player == "scissors"):
+	if (CPU == "rock"):
+		print("you lose!")
+	else:
+		print("you win!")
+#choose the winner
+
+#print the outcome
